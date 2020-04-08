a8e196a911bebda78365859a6a71d2e87c9ff912
Terry Schneider <tschneider@firebend.com>
2020-04-08 16:37:14 -0500

    add install script

---
+++ install.bat
@@ -0,0 +1,4 @@
+mklink "%HOMEPATH%\.bash_profile" "%0\..\.bash_profile"
+mklink "%HOMEPATH%\.gitconfig" "%0\..\.gitconfig"
+mklink "%HOMEPATH%\.bashrc" "%0\..\.bashrc"
+mklink "%HOMEPATH%\Documents\WindowsPowerShell\profile.ps1" "%0\..\Documents\WindowsPowerShell\profile.ps1"
