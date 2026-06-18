d465c1b32d8fda505b4c8c84da4ebb02fd711c90
Terry Schneider <tschneider@firebend.com>
2026-06-18 02:00:36 -0500

    feat: add timezone conversion scripts and update plugin configurations

--- .zsh/plugins/utils/utils.plugin.zsh
+++ .zsh/plugins/utils/utils.plugin.zsh
@@ -2,8 +2,9 @@
 
 _plugin_dir="${0:A:h}"
 fpath=("$_plugin_dir" $fpath)
-autoload -Uz anykey keepalive profile repo _repo
+autoload -Uz anykey keepalive profile repo _repo tzconv _tzconv
 
 if (( $+functions[compdef] )); then
   compdef _repo repo
+  compdef _tzconv tzconv
 fi
